f90bebe0cef3102dced7a785caf2b100d96c2642
aravind <aravindlakshmanan007@gmail.com>
2019-02-04 20:12:20 +0530

    Initial Commit

---
+++ labex5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  labex5
+//
+//  Created by Aravind on 04/02/19.
+//  Copyright © 2019 Aravind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
